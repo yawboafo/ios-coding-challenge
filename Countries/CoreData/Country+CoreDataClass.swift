0a18081ffa1be6cd815d913216588dcee01f687c
TrueLite <yawboafo77@gmail.com>
2022-04-01 04:37:12 +0000

    CoreData managedObject Modified

---
+++ Countries/CoreData/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  
+//
+//  Created by Yaw Koranteng Boafo on 01/04/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
--- Countries/CoreData/Country+CoreDataProperties.swift
+++ Countries/CoreData/Country+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Country+CoreDataProperties.swift
 //  
 //
-//  Created by Syft on 04/03/2020.
+//  Created by Yaw Koranteng Boafo on 01/04/2022.
 //
 //
 
@@ -19,5 +19,7 @@ extension Country {
     @NSManaged public var capital: String?
     @NSManaged public var name: String?
     @NSManaged public var population: Int32
+    @NSManaged public var area: Double
+    @NSManaged public var region: String?
 
 }
